39ee56a3bdd4b4949ae12e01da4ed73e0dc5a870
liuyangc3 <liuyangc33@gmail.com>
2017-01-18 13:36:27 +0800

    lua parser

--- test.lua
+++ test.lua
@@ -1,7 +1,2 @@
-local path = "C:\\Users\\web\\PycharmProjects\\openresty-lua-script\\config_parser\\"
-local m_package_path = package.path
-package.path = string.format("%s;%s?.lua;", m_package_path, path, path)
-
-
 local p = require("parser")
 p.print_t(p.parse("server.conf"))
